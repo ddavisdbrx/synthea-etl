55fb4db30189e41762e6f09bf53a0bca4075536b
ddavisdbrx <ddavisdbrx>
2024-06-18 15:45:01 +0000

    updated gold tables

--- synthea_etl/src/03_notebook_gold_table.py
+++ synthea_etl/src/03_notebook_gold_table.py
@@ -1,28 +1,48 @@
 # Databricks notebook source
+# define widgets/parameters
 dbutils.widgets.text("environment", "dev")
+dbutils.widgets.text("debug", "true")
+
+# assign widget values to variaables
 environment = "_" + dbutils.widgets.get("environment")
+debug = eval(dbutils.widgets.get('debug').title())
 print(f"Target environment: {dbutils.widgets.get('environment')}")
+print(f"Debug run: {debug}")
 
 # COMMAND ----------
 
-
+# create temp table
 spark.sql(f"""
 CREATE OR REPLACE TEMP VIEW source
 AS
-SELECT count(*) as encounters, cast(start as date) as encounter_date
-FROM ddavis_demo{environment}.ddavis_encounters.silver_encounters;
+SELECT count(*) as encounters, cast(start_datetime as date) as encounter_date
+FROM ddavis_demo{environment}.ddavis_encounters.silver_encounters
+GROUP BY cast(start_datetime as date);
 """)
+
+# debug
+if debug == True: 
+  spark.table(f"source").display()
 
 # COMMAND ----------
 
+# create table if not exists
 spark.sql(f"""
 -- create table if not exists          
-CREATE TABLE IF NOT EXISTS  ddavis_demo{environment}.ddavis_encounters.gold_aggregae_encounters;
+CREATE TABLE IF NOT EXISTS  ddavis_demo{environment}.ddavis_encounters.gold_aggregate_encounters
+AS SELECT * FROM source;
+""")
 
---merge updates
-MERGE INTO ddavis_demo{environment}.ddavis_encounters.gold_aggregae_encounters as target
+
+# upsert updates
+spark.sql(f"""
+MERGE INTO ddavis_demo{environment}.ddavis_encounters.gold_aggregate_encounters as target
 USING source
-ON target.encounter_date = source.target_date
+ON target.encounter_date = source.encounter_date
 WHEN MATCHED THEN UPDATE SET *
 WHEN NOT MATCHED THEN INSERT *;
 """)
+
+# debug
+if debug == True: 
+  spark.table(f"ddavis_demo{environment}.ddavis_encounters.gold_aggregate_encounters").display()
